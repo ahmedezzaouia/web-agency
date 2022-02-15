89f36c1ed576869247ec5b79550ee8365e6f9416
Ahmed Ez zaouia <ahmez-za@e1r10p1.1337.ma>
2022-02-15 17:23:41 +0100

    page width

--- src/components/header/hero/hero.jsx
+++ src/components/header/hero/hero.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import "./styles.scss";
 const Hero = () => {
-  return <div className="hero"></div>;
+  return (
+    <div className="hero">
+      <div className="hero-content">
+        <span>MARKETS & RESOURCES</span>
+        <h1>We Take Care Your Business Growth</h1>
+        <button className="btn-quote">Get a Quote</button>
+      </div>
+      <div className="hero-img"></div>
+    </div>
+  );
 };
 
 export default Hero;
