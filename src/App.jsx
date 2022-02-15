49f15322881e1dfd5264d66968775c9c5bc52fc3
Ahmed Ez zaouia <ahmez-za@e1r9p1.1337.ma>
2022-02-15 01:49:56 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Home from "./pages/home";
+function App() {
+  return (
+    <h1>
+      <Home />
+    </h1>
+  );
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+import Navbar from "./navigation/navbar";
+
+export { Navbar };
