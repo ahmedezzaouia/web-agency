dcbf2bcf389c561b2990e8addfce763c318bc887
Ahmed Ez zaouia <ahmez-za@e1r9p1.1337.ma>
2022-02-15 01:59:23 +0100

    navigation init

--- src/components/navigation/navbar.jsx
+++ src/components/navigation/navbar.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import "./styles.css";
 const Navbar = () => {
-  return <div>Navigation</div>;
+  return (
+    <div className="navigation-area">
+      <div className="brand-logo"></div>
+      <div className="primary-area">
+        <div className="top-navigation"></div>
+        <div className="nav-menu"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
