4df2bdb6948f7823aca0ec1c3572d2dfe705ce4b
Ahmed Ez zaouia <ahmez-za@e1r10p10.1337.ma>
2022-02-16 18:49:51 +0100

    complete feature section layout

--- src/components/careFeatures/features.jsx
+++ src/components/careFeatures/features.jsx
@@ -4,14 +4,43 @@ import "./styles.scss";
 const Features = () => {
   return (
     <div className="features-container">
-      <div className="feature-image"></div>
+      <div className="feature-image">
+        <div className="image-container">
+          <img src="https://webtend.net/demo/html/datapro/assets/img/about-us.jpg" alt="professional" />
+        </div>
+      </div>
       <div className="features-content">
-        <span>CARE FEATURES</span>
-        <h2>We Are A Professional Creative Agency</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at est id leo luctus gravida a in ipsum. Vivamus
-          vel molestie nisi. Aliquam maximus maximus velit, id scelerisque urna elem
-        </p>
+        <div className="container">
+          <span>CARE FEATURES</span>
+          <h2>We Are A Professional Creative Agency</h2>
+          <p className="featuur-para">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at est id leo luctus gravida a in ipsum.
+            Vivamus vel molestie nisi. Aliquam maximus maximus velit, id scelerisque urna elem
+          </p>
+          {/* item feature  */}
+          <div className="feature-item">
+            <div className="check-box">
+              <i class="fa-solid fa-check"></i>
+            </div>
+            <div className="feature-item-content">
+              <h5>Ligula molestie</h5>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+              </p>
+            </div>
+          </div>
+          <div className="feature-item">
+            <div className="check-box">
+              <i class="fa-solid fa-check"></i>
+            </div>
+            <div className="feature-item-content">
+              <h5>Ligula molestie</h5>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
